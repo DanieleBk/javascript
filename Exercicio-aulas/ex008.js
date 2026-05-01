8467c277f0ceeaa78b524482042506175d9cda35
DanieleBk <danibroboski@outlook.com>
2026-05-01 19:46:50 -0300

    Create ex008.js
    atualizado

---
+++ Exercicio-aulas/ex008.js
@@ -0,0 +1,7 @@
+var vel = 62
+console.log ('A sua velocidade é ' + vel + 'km/h.')
+if (vel > 60) {
+    console.log ('Você ultrapassou a velocidade permitida. Foi multado!')
+}
+
+console.log ('Dirija sempre com cuidado')
