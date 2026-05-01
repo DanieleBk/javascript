17f0bf64b7e824dc1012f1bcd669f3c4caaa83ea
DanieleBk <danibroboski@outlook.com>
2026-05-01 19:45:41 -0300

    Create node_0cc0bd2fc2f47.tmp.js
    atualizado

---
+++ Exercicio-aulas/ambienteJS-aula 13/node_0cc0bd2fc2f47.tmp.js
@@ -0,0 +1,37 @@
+contador = 1
+do {
+    console.log(`Olá ${contador}`)
+    contador +2
+} while (contador <= 8)
+
+
+
+
+/*var c = 1
+do {
+    console.log (`Passo ${c}. Tudo bem?`)
+    c++
+} while (c <= 8)*/
+
+
+
+
+/*var c = 1
+while (c <= 8) {
+    console.log (`Passo ${c}. Tudo bem?`)
+    c++
+}*/
+
+
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
