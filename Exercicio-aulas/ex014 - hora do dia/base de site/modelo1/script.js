01339f3c14acadf105b89317faeb31e40b6ca600
DanieleBk <danibroboski@outlook.com>
2026-05-01 19:47:56 -0300

    Create script.js
    atualizado

---
+++ Exercicio-aulas/ex014 - hora do dia/base de site/modelo1/script.js
@@ -0,0 +1,11 @@
+function clicar(){
+    var ini = window.document.querySelector('input#num')
+    var fim = window.document.querySelector(input#fim)
+    var intervalo = window.document.querySelector ('input#intervalo')
+
+    if (ini.value.length == 0){
+        window.alert ('preencha o valor')
+    } else {
+        window.alert ('ok')
+    }
+}
